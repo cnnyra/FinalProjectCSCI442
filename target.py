606b75faafc8a32a58621563e053e98323278a7f
cnnyra <collinsaan@gmail.com>
2019-04-25 10:03:17 -0600

    Fix finding target color, adjusted to use it in main

--- target.py
+++ target.py
@@ -7,16 +7,16 @@ import cv2
 # pink hsv min: 153, 96,  142
 # pink hsv max: 170, 179, 220
 
-def frame_contains_pink_target(frame):
-    mask = cv2.inRange(frame, (153, 96, 142), (170, 179, 220))
+def frameContainsTargetColor(frame, targetColorLow, targetColorHigh):
+    mask = cv2.inRange(frame, targetColorLow, targetColorHigh)
     width = int(mask.shape[1] * 2/5)
     middle = mask[:, width : mask.shape[1] - width]
     return 255 in middle
 
-def frame_contains_white_border(frame):
+def frameContainsWhiteBorder(frame):
     pass
 
-def get_white_blobs(frame):
+def getWhiteBlobs(frame):
     
 
 def main():
@@ -35,7 +35,7 @@ def main():
         # and occupied/unoccupied text
         image = frame.array
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        print(frame_contains_pink_target(hsv))
+        print(frameContainsTargetColor(hsv))
         # show the frame
         cv2.imshow("Frame", image)
         key = cv2.waitKey(1) & 0xFF
